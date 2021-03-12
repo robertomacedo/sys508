141ade22ec56af0a60963f7423b90a3a22e86723
robertomacedo <macedorss20@gmail.com>
2021-03-12 16:14:03 -0300

    criada pasta de arquivos static

--- paginas/views.py
+++ paginas/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
--- sytemcadchild/urls.py
+++ sytemcadchild/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('paginas.urls')),
+    path('', include('paginas.urls')),
+    
 ]

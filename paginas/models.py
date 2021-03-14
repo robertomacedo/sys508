192be27129ed5c51cbdf906283f3c73406ca705c
robertomacedo <macedorss20@gmail.com>
2021-03-14 15:07:44 -0300

    Alerações no prjeto exclusão mod cadastro

--- paginas/models.py
+++ paginas/models.py
@@ -3,11 +3,10 @@ from django.db import models
 
 class Pessoa(models.Model):
     name = models.CharField(max_length=50, blank=False)
-    nascimento = models.DateField(blank=True)
+    nascimento = models.DateField(blank=True, null=True)
     contato = models.CharField(max_length=15)
     mae = models.CharField(max_length=50, blank=False)
     pai = models.CharField(max_length=50, blank=True, null=True)
-    foto = models.ImageField(blank=True)
     datecadastro = models.DateTimeField(auto_now_add=True)
     dateupgrade = models.DateTimeField(auto_now=True)
 
